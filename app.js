a76496e047dec3490b826c3b30f5364382c1abed
Kyungjin Jung <okyungjin@gmail.com>
2023-08-31 15:31:03 +0900

    #1.3 moveTo and lineTo

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.stroke();
-
-ctx.strokeStyle = 'red';
-setTimeout(() => {
-  ctx.stroke();
-}, 2000);
-
-ctx.beginPath();
-ctx.rect(450, 450, 100, 100);
-ctx.rect(550, 550, 100, 100);
-ctx.fillStyle = 'red';
-ctx.fill();
