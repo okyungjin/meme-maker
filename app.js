80f7c37bab3f996212c18ec6873aff2dc8d95b47
Kyungjin Jung <okyungjin@gmail.com>
2023-09-01 15:23:53 +0900

    #1.5 Drawing Project Two

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(170, 200, 15, 100);
+ctx.fillRect(310, 200, 15, 100);
+ctx.fillRect(220, 200, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.arc(270, 80, 5, 0, 2 * Math.PI);
+ctx.arc(230, 80, 5, 0, 2 * Math.PI);
 ctx.fill();
