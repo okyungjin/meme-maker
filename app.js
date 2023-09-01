961dd9cd66807ed6f27dc4bf6ef222cf38508fde
Kyungjin Jung <okyungjin@gmail.com>
2023-09-01 15:46:28 +0900

    #2.1 Mouse Painting

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
-ctx.moveTo(0, 0);
+let isPainting = false;
 
-const colors = [
-  '#ff3838',
-  '#ffb8b8',
-  '#c56cf0',
-  '#ff9f1a',
-  '#fff200',
-  '#32ff7e',
-  '#7efff5',
-];
-
-const onCanvasMouseMove = (event) => {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+const onMouseMove = (event) => {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 };
 
-canvas.addEventListener('mousemove', onCanvasMouseMove);
+const startPainting = () => {
+  isPainting = true;
+};
+
+const cancelPainting = () => {
+  isPainting = false;
+};
+
+canvas.addEventListener('mousemove', onMouseMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
